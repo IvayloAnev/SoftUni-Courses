45d8e5954005437a99954bf9980bc43c5a62a60a
IvayloAnev <105644851+IvayloAnev@users.noreply.github.com>
2022-05-18 18:51:18 +0300

    Add files via upload

---
+++ Basic Syntax - Lab/01.multiplyNumberBy2.js
@@ -0,0 +1,9 @@
+function slove (num) {
+let result = num*2
+console.log(result)
+}
+
+slove (2)
+slove (5)
+
+//100/100
---
+++ Basic Syntax - Lab/08.divisibleBy3.js
@@ -0,0 +1,17 @@
+function solve(){
+for(let i=3 ; i<=100 ; i+=3){
+    console.log(i)
+}
+}
+solve()
+//100/100
+
+function solve1(){
+    for(i=1 ; i<=100 ; i++){
+        if(i%3==0){
+            console.log(i)
+        }
+    }
+}
+solve1()
+//100/100
