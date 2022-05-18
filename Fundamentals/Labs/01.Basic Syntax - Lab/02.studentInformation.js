3295d516eb6d263a72ae19dda103fde9cfabf183
IvayloAnev <105644851+IvayloAnev@users.noreply.github.com>
2022-05-18 19:00:34 +0300

    Add files via upload

---
+++ Fundamentals/Labs/01.Basic Syntax - Lab/02.studentInformation.js
@@ -0,0 +1,7 @@
+function slove(name , age , grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+slove ('John', 15, 5.54678)
+slove ('Steve', 16, 2.1426)
+
+//100/100
