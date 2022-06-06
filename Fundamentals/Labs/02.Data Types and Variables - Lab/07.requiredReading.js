cfebdbcb481b2889c5100700fdab9c827426c450
IvayloAnev <105644851+IvayloAnev@users.noreply.github.com>
2022-06-06 17:28:30 +0300

    Add files via upload

---
+++ Fundamentals/Labs/02.Data Types and Variables - Lab/07.requiredReading.js
@@ -0,0 +1,11 @@
+function solve(a, b, c) {
+    let numberOfPages = a;
+    let pagesReadInOneHour = b;
+    let days = c;
+    let neededHours = numberOfPages / pagesReadInOneHour;
+    let neededHoursADay = neededHours / days;
+    console.log(neededHoursADay);
+}
+solve(432, 15, 4);
+
+//100/100;
